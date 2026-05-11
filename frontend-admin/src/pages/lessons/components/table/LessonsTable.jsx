a477151b0f3647e338adc0269e688de8237f7389
sseezov <sseezov@mail.ru>
2026-05-11 09:13:01 +0700

    front-admin: add select group in lessons table

--- frontend-admin/src/pages/lessons/components/table/LessonsTable.jsx
+++ frontend-admin/src/pages/lessons/components/table/LessonsTable.jsx
@@ -1,17 +1,20 @@
+import state from '../../../../state';
 import { daysMap } from '../../../../utils/lessons';
 import HeadCell from './HeadCell';
 import styles from './LessonsTable.module.css'
 import TableCell from './TableCell';
 
+
 export default function LessonsTable({ groups, schedule }) {
   const { weekdays, lessonsInDay } = schedule
+  const { selectedGroup } = state.ui
 
   return (
     <table class={styles.table}>
       <thead>
         <tr>
           <th>День недели</th>
-          
+
           {weekdays.map((day, idx) => (
             <th>{daysMap[day]}</th>
           ))}
@@ -19,16 +22,16 @@ export default function LessonsTable({ groups, schedule }) {
         <tr>
           <th>Пара</th>
           {weekdays.map((day, idx) => (
-              <HeadCell lessonsInDay={lessonsInDay}/>
-            ))}
+            <HeadCell lessonsInDay={lessonsInDay} />
+          ))}
         </tr>
       </thead>
       <tbody>
         {groups.map((group) => (
-          <tr>
+          <tr class={selectedGroup === group.id ? 'selectedGroup' : ''}>
             <td>{group.name}</td>
             {weekdays.map((day, idx) => (
-              <TableCell lessonsInDay={lessonsInDay}/>
+              <TableCell lessonsInDay={lessonsInDay} />
             ))}
           </tr>
         ))}
